75e9794fd11accbd870b4f250715a4b4b1649174
unknown <daniriian@gmail.com>
2021-01-30 15:39:33 +0200

    Added SimpleHero component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,41 @@
 import React from "react"
 
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
+
+import { Link } from "gatsby"
+
+import styles from "../css/footer.module.css"
+
 const Footer = () => {
   return (
-    <div>
-      <h1>I am footer</h1>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {links.map((item, index) => {
+          return (
+            <Link to={item.path} key={index}>
+              {item.text}
+            </Link>
+          )
+        })}
+      </div>
+      <div className={styles.icons}>
+        {socialIcons.map((item, index) => (
+          <a
+            key={index}
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {item.icon}
+          </a>
+        ))}
+      </div>
+      <div className={styles.copyright}>
+        copyright &copy; backroads travel company {new Date().getFullYear()} all
+        rights reserved
+      </div>
+    </footer>
   )
 }
 
