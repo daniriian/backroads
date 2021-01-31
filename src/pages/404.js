3369bd501a754532f503a2e951deb97d0bc95d64
unknown <daniriian@gmail.com>
2021-01-31 17:48:04 +0200

    home page done

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,20 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styles from "../css/error.module.css"
 
-const error = () => {
-  return <Layout>Hello from 404 Page</Layout>
+import { Link } from "gatsby"
+import Banner from "../components/Banner"
+
+export default function error() {
+  return (
+    <Layout>
+      <header className={styles.error}>
+        <Banner title="oops its'a dead end">
+          <Link to="/" className="btn-white">
+            back to homepage
+          </Link>
+        </Banner>
+      </header>
+    </Layout>
+  )
 }
-
-export default error
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,30 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import Banner from "../components/Banner"
 
 import SimpleHero from "../components/SimpleHero"
+import About from "../components/Home/About"
+import Services from "../components/Home/Services"
 
-export default function Home() {
+const Home = () => {
   return (
     <Layout>
-      <SimpleHero />
+      <SimpleHero>
+        <Banner
+          title="continue exploring"
+          info="In eiusmod consectetur id do excepteur ullamco nisi minim nostrud irure
+        magna magna in."
+        >
+          <Link to="/tours" className="btn-white">
+            explore tours
+          </Link>
+        </Banner>
+      </SimpleHero>
+      <About />
+      <Services />
     </Layout>
   )
 }
+
+export default Home
